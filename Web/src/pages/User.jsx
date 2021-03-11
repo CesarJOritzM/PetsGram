2107462239df6facb5efa4458a35908056b9b64c
CesarJOritzM <cesarjavierortizmontero@gmail.com>
2021-03-11 01:29:27 -0500

    Finish user session

--- Web/src/pages/User.jsx
+++ Web/src/pages/User.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { Context } from '../hooks/Context';
 
-const User = () => <div>User</div>;
+const Button = styled.button`
+  background: #8d00ff;
+  border-radius: 3px;
+  color: #fff;
+  height: 32px;
+  display: block;
+  width: 100%;
+  text-align: center;
+  &[disabled] {
+    opacity: 0.3;
+  }
+`;
+const User = () => {
+  const { removeAuth } = useContext(Context);
+  return (
+    <>
+      <h1>User</h1>
+      <Button onClick={removeAuth}>Cerrar sesión</Button>
+    </>
+  );
+};
 
 export default User;
